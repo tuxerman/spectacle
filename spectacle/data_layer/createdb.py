412191cb571e04392eb0171283e36edc7cb1cfe4
Sriram Padmanabhan <spzone@gmail.com>
2016-11-13 22:55:59 -0800

    Added create/delete table functions for both primary and FTS index

--- spectacle/data_layer/createdb.py
+++ spectacle/data_layer/createdb.py
@@ -2,14 +2,36 @@
 """
 Create table
 """
+import simplejson as json
+from elasticsearch import Elasticsearch
 
 from spectacle.data_layer.database_setup import CURRENT_DATABASE
 from spectacle.data_layer.document_data import Document
+from spectacle.data_layer.database_setup import ES_INDEX_MAPPING
+from spectacle.data_layer.full_text_search import INDEX as FTS_INDEX
+from spectacle.data_layer.full_text_search import HOST, PORT
 
 
-def create_tables(database):
+def delete_fts_index(es_host, es_port, es_index):
+    es = Elasticsearch([{'host': es_host, 'port': es_port}])
+    es.indices.delete(index=es_index, ignore=[400, 404])
+
+
+def create_primary_tables(database):
     database.connect()
     database.create_tables([Document])
 
 
-create_tables(CURRENT_DATABASE)
+def delete_primary_tables():
+    Document.drop_table(fail_silently=True)
+
+
+def create_fts_index(es_host, es_port, es_index, es_index_mapping):
+    es = Elasticsearch([{'host': es_host, 'port': es_port}])
+    es.indices.create(index=es_index, ignore=400, body=json.dumps(es_index_mapping))
+
+
+delete_primary_tables()
+create_primary_tables(CURRENT_DATABASE)
+delete_fts_index(HOST, PORT, FTS_INDEX)
+create_fts_index(HOST, PORT, FTS_INDEX, ES_INDEX_MAPPING)
